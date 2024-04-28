d2f9aa6319247577a73804291087103f7ac97e94
iwantacrepe <100364726+iwantacrepe@users.noreply.github.com>
2024-04-28 15:34:00 +0530

    Update tp_lcm.m

--- tp_lcm.m
+++ tp_lcm.m
@@ -1,62 +1,50 @@
 clc
 clear all
-
-c=[4 2 3 5 6; 1 9 4 1 8;3 1 4 2 7;5 3 8 6 4];
-a=[4 5 9 1 2];
-b=[7 9 5 8 4];
-sum(a)
-sum(b)
-m= size(c,1);
-n= size(c,2);
-
-z=0;
-
-if sum(a) == sum(b)
-    fprintf('balanced')
+Cost=[11 13 17 14;16 18 14 10;21 24 13 10]
+A=[250 300 400];
+B=[200 225 275 250];
+if sum(A) == sum(B)
+    fprintf('Given transportation problem is balanced')
 else
-     fprintf('unbalanced')
-if sum(a)<sum(b)
-    c(end+1,:) = zeros(1,length(b))
-    a(end+1) = sum(b) - sum(a)
-    
+    fprintf('Given transportation problem is unbalanced')
+    if sum(A) < sum(B)
+        Cost(end+1, :) = zeros(1, size(A,2));
+        A(end+1) = sum(B) - sum(A);
+    elseif sum(B) < sum(A)
+        Cost(:, end+1) = zeros(1, size(A, 2));
+        B(end+1) = sum(A) - sum(B);
+    end
+end
+
+Icost = Cost;
+X = zeros(size(Cost));
+[m, n] = size(Cost);
+BFS = m + n - 1;
+
+for i=1:m
+    for j=1:n
+        cs = min(Cost(:));
+        [RowIndex, ColIndex] = find(cs == Cost);
+        x11 = min(A(RowIndex), B(ColIndex));
+        [value, Index] = max(x11);
+        ii = RowIndex(Index);
+        jj = ColIndex(Index);
+        y11 = min(A(ii), B(jj));
+        X(ii, jj) = y11;
+        A(ii) = A(ii) - y11;
+        B(jj) = B(jj) - y11;
+        Cost(ii, jj) = Inf;
+    end
+end
+
+fprintf('Initial BFS\n');
+IBFS = array2table(X);
+disp(IBFS);
+TotalBFS = length(nonzeros(X));
+if TotalBFS == BFS
+    fprintf('Initial BFS is non degenerate\n')
 else
-    c(:,end+1)=zeros(length(a),1);
-    b(end+1)=sum(a)-sum(b);
-end
+    fprintf('Initial BFS is degenerate\n')
 end
 
-x=zeros(m,n);
-initial_c = c;
-for i=1:size(c,1)
-    for j=1:size(c,2)
-       cpq = min(c(:))
-       if cpq == inf
-        break
-    end
-    [p1 q1] = find(cpq ==c)
-
-    xpq =min(a(p1), b(q1))
-    [x1 ind] = max(xpq)
-    p=p1(ind)
-    q=q1(ind)
-
-    x(p,q) = min(a(p),b(q))
-
-    if min(a(p), b(q)) == a(p)
-        b(q) = b(q)-a(p)
-        a(p) = a(p)-x(p,q)
-        c(p,:) = inf
-    else
-        a(p) = a(p) - b(q)
-         b(q)= b(q) - x(p,q)
-         c(:,q) = inf
-    end
-    end
-end
-for i=1:size(c,1)
-    for j=1:size(c,2)
-        z= z+ initial_c(i,j)*x(i,j)
-    end
-end
-array2table(x)
-fprintf('TP is %f\n',z)
+InitialCost = sum(sum(Icost .* X)) 
